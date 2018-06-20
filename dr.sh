80c001240bb73498b39f3e1d832df9654a1e105c
Marcus Nyeholt <marcus@symbiote.com.au>
2018-06-20 11:26:06 +1000

    new(dr.sh) Added container exec command to run a bash-wrapped command, useful for CI integration

--- dr.sh
+++ dr.sh
@@ -4,6 +4,7 @@ CMD=$1
 CONTAINER="none"
 ACTION="pass"
 RUN_OPTS="-it"
+CONTAINER_PREFIX=$(basename $(pwd))
 YARN_DIR=EDIT_THIS_PLEASE
 
 case "$CMD"
@@ -54,31 +55,53 @@ case "$2"
 in
     cli) 
         ACTION="cli"
-    ;;
+        ;;
+    exec) 
+        ACTION="exec"
+        ;;
 esac
 
 if [ $CONTAINER = "none" ]; then
-    echo "Valid containers are php, fpm, mysql, sel, yarn"
+    echo "Please provide a container or a command to be run - ./dr.sh {container|command} [arguments] "
+    echo "Available containers are as follows; any arguments will be used as docker exec parameters : "
+    echo "  php, fpm, mysql, node"
+    echo "Available commands are below; any arguments are passed to these commands in the container "
+    echo "  composer, phing, codecept, mysqlimport, yarn"
+
     exit 1;
 fi
 
+# check whether we're running in CI on gitlab
+if [ ! -z "${CI}" ]; then
+    # noop
+    RUN_OPTS="-i"
+    echo "Executing in CI, setting to no TTY"
+fi
+
+CONTAINER_NAME=${CONTAINER_PREFIX}_${CONTAINER}_1
 
 if [ $ACTION = "cli" ]; then
     echo "Dropping to shell in $CONTAINER"
-    docker exec -it -u `id -u`:`id -g` $(basename $(pwd))_${CONTAINER}_1 /bin/bash
+    docker exec ${RUN_OPTS} -u `id -u`:`id -g` ${CONTAINER_NAME} /bin/bash
+elif [ $ACTION = "exec" ]; then
+    # double shift
+    shift
+    shift
+    echo "Execute bash wrapped command on $CONTAINER"
+    docker exec ${RUN_OPTS} -u `id -u`:`id -g` ${CONTAINER_NAME} bash -c "$@"
 else
     shift
     echo "Running command $ACTION in $CONTAINER"
     if [ "mysqlimport" = $CMD ]; then
-        docker exec ${RUN_OPTS} -u `id -u`:`id -g` $(basename $(pwd))_${CONTAINER}_1 ${ACTION} "$@" < /proc/$$/fd/0
+        docker exec ${RUN_OPTS} -u `id -u`:`id -g` ${CONTAINER_NAME} ${ACTION} "$@" < /proc/$$/fd/0
     elif [ "yarn" = $CMD ]; then
-        if [ "EDIT_THIS_PLEASE" = $YARN_DIR ] then
+        if [ "EDIT_THIS_PLEASE" = $YARN_DIR ]; then
             echo "Please update dr.sh and change the YARN_DIR value"
             exit 1;
-        else 
-            docker exec ${RUN_OPTS} -u `id -u`:`id -g` $(basename $(pwd))_${CONTAINER}_1 bash -c "cd $YARN_DIR; yarn $@"
+        else
+            docker exec ${RUN_OPTS} -u `id -u`:`id -g` ${CONTAINER_NAME} bash -c "cd $YARN_DIR; yarn $@"
         fi
     else
-        docker exec ${RUN_OPTS} -u `id -u`:`id -g` $(basename $(pwd))_${CONTAINER}_1 ${ACTION} "$@"
+        docker exec ${RUN_OPTS} -u `id -u`:`id -g` ${CONTAINER_NAME} ${ACTION} "$@"
     fi
 fi
