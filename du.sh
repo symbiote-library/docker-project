8e40eb66edc4c00fff7c52a57e9ba1261781f9fa
Marcus Nyeholt <marcus@symbiote.com.au>
2018-07-04 14:44:52 +1000

    new(solr) If solr is installed, make sure its parent folder is created with owner of solr:solr

--- du.sh
+++ du.sh
@@ -6,6 +6,18 @@ if [ -z "$DOCKER_SHARED_PATH" ]; then
     echo "Setting default shared path to $DOCKER_SHARED_PATH "
 fi
 
+case "$CONTAINERS" in 
+    *solr*)
+        if [ -d ${DOCKER_SHARED_PATH}/solr-data ]; then
+            echo "Solr data dir found, if solr does _NOT_ start, please sudo chown 8983:8983 ${DOCKER_SHARED_PATH}/solr-data"
+        else 
+            echo "Creating solr data-dir, sudo chown'd as the solr user (8983)"
+            mkdir ${DOCKER_SHARED_PATH}/solr-data
+            sudo chown 8983:8983 ${DOCKER_SHARED_PATH}/solr-data
+        fi
+    ;;
+esac
+
 export DOCKER_SHARED_PATH=$DOCKER_SHARED_PATH
 
 echo "Creating services: ${CONTAINERS}"
