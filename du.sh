39ff46da52ea91fd02aec9a02ed958656da4c6fe
jcarter <josh@symbiote.com.au>
2018-08-13 16:40:52 +1000

    [Add] DOCKER_PROJECT_PATH and associated changes.
    - Project specific dir now defined in du.sh
    - Can be overriden in .env same as shared path.
    - Is `DOCKER_SHARED_PATH/(basename pwd)` by default.
    - Changed ALL volumes to use project path with exception for composer cache.
    - MYSQL volume name made generic again and comment about 5-7 removed.

--- du.sh
+++ du.sh
@@ -2,24 +2,29 @@
 CONTAINERS="apache php phpcli adminer mysql56 selenium mailhog node"
 
 if [ -z "$DOCKER_SHARED_PATH" ]; then
-    DIR=$(basename "$PWD")
-    DOCKER_SHARED_PATH=~/docker/${DIR}
+    DOCKER_SHARED_PATH=~/docker
     echo "Setting default shared path to $DOCKER_SHARED_PATH "
+fi
+
+if [ -z "$DOCKER_PROJECT_PATH" ]; then
+    DOCKER_PROJECT_PATH=${DOCKER_SHARED_PATH}/$(basename $(pwd))
+    echo "Setting default project data path to $DOCKER_PROJECT_PATH "
 fi
 
 case "$CONTAINERS" in 
     *solr*)
-        if [ -d ${DOCKER_SHARED_PATH}/solr-data ]; then
+        if [ -d ${DOCKER_PROJECT_PATH}/solr-data ]; then
             echo "Solr data dir found, if solr does _NOT_ start, please sudo chown 8983:8983 ${DOCKER_SHARED_PATH}/solr-data"
         else 
             echo "Creating solr data-dir, sudo chown'd as the solr user (8983)"
-            mkdir ${DOCKER_SHARED_PATH}/solr-data
-            sudo chown 8983:8983 ${DOCKER_SHARED_PATH}/solr-data
+            mkdir ${DOCKER_PROJECT_PATH}/solr-data
+            sudo chown 8983:8983 ${DOCKER_PROJECT_PATH}/solr-data
         fi
     ;;
 esac
 
 export DOCKER_SHARED_PATH=$DOCKER_SHARED_PATH
+export DOCKER_PROJECT_PATH=$DOCKER_PROJECT_PATH
 
 echo "Creating services: ${CONTAINERS}"
 docker-compose up -d ${CONTAINERS}
