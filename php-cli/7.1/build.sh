0dad7e702314e6d9380beb6e3ad5ec306610885b
Marcus Nyeholt <marcus@symbiote.com.au>
2020-01-04 16:03:40 +1100

    new(php) Added php 7.3 containers
    * Change to 7.3 as the base
    * Change to compile / docker-php-ext-install for tideways non
    commercials
    * Changed to a working config for the msmtp config

---
+++ php-cli/7.1/build.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+docker build -t symbiote/php-cli:7.1 .
--- php/build.sh
+++ php/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-docker build -t symbiote/php-fpm:7.1 .
+docker build -t symbiote/php-fpm:7.3 .
 
 # and the latest tag also
 docker build -t symbiote/php-fpm .
